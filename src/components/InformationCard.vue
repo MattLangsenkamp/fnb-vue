e9c7af56547176cc15d4339006797b5dbb21181f
mattlangsenkamp <langsenkamp.4@osu.edu>
2020-12-15 12:46:18 -0500

    yep

--- src/components/InformationCard.vue
+++ src/components/InformationCard.vue
@@ -1,9 +1,26 @@
 <template>
-  <div></div>
+  <div class="containe" r>
+    <div class="top-bar"></div>
+    <div class="bottom-bar"></div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "InformationCard",
-};
+  name: 'InformationCard'
+}
 </script>
+
+<style scoped>
+.container {
+  height: 2em;
+  width: 3.236em;
+}
+.top-bar {
+  background-color: green;
+}
+
+.bottom-bar {
+  background-color: greenyellow;
+}
+</style>
