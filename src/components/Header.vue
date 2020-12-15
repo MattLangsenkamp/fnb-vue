ec7b993b41c452f58c069e75a49f348418c1814d
mattlangsenkamp <langsenkamp.4@osu.edu>
2020-12-15 12:20:01 -0500

    yep

---
+++ src/components/Header.vue
@@ -0,0 +1,123 @@
+<template>
+  <div id="nav">
+    <img class="logo" alt="Roc Food Not Bombs" src="../assets/rfnb.svg" />
+    <div class="links">
+      <div class="dropdown">
+        <img
+          class="user-img"
+          alt="default-login-img"
+          src="../assets/login-default.svg"
+        />
+        <div class="dropdown-content">
+          <router-link class="nav-link-embedded" to="/signin"
+            >sign in</router-link
+          >
+          <router-link class="nav-link-embedded" to="/signup"
+            >sign up</router-link
+          >
+        </div>
+      </div>
+      <router-link to="/locations">locations</router-link>
+      <router-link to="/">about</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data() {
+    return {
+      fetchedImage: ''
+    }
+  },
+  computed: {
+    loginImage() {
+      return this.fetchedImage ? 'poo' : '../assets/rfnb.svg'
+    }
+  }
+}
+</script>
+<style scoped>
+#nav {
+  border-bottom: 1px solid black;
+  height: 3em;
+  flex-direction: row;
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 5.5em;
+}
+.user-img {
+  height: 2.4em;
+  margin: 1em;
+}
+
+#nav a:hover,
+.user-img:hover {
+  transform: scale(1.1);
+  text-shadow: 0px 0px 2px rgba(0, 0, 0, 0.6);
+}
+
+.user-img:hover {
+  border-radius: 1.2em;
+
+  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.6);
+  -moz-box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.6);
+  -webkit-box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.6);
+  -o-box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.6);
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+.logo {
+  position: ablsolute;
+  height: 10em;
+  display: none;
+}
+@media screen and (min-width: 540px) {
+  .logo {
+    display: inline;
+  }
+}
+
+.logo:hover {
+  transform: scale(1.1);
+}
+
+.links {
+  width: 100%;
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+}
+
+#nav a {
+  font-weight: bold;
+  font-size: 1.5rem;
+  color: black;
+  margin: 1em;
+  text-decoration: none;
+}
+
+#nav a.router-link-exact-active {
+  color: #b9519e;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  margin-left: -5.5em;
+  background-color: #e4e4e4;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.4);
+  z-index: 1;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,47 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import Locations from "../views/Locations.vue";
+import About from "../views/About.vue";
+import SignUp from "../views/SignUp.vue";
+import SignIn from "../views/SignIn.vue";
+import Profile from "../views/Profile.vue";
+import LocationPage from "../views/LocationPage.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "About",
+    component: About,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+    path: "/locations",
+    name: "Locations",
+    component: Locations,
+  },
+  {
+    path: "/sign-up",
+    name: "SignUp",
+    component: SignUp,
+  },
+  {
+    path: "/sign-in",
+    name: "SignIn",
+    component: SignIn,
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+  },
+  {
+    path: "/location-page",
+    name: "LocationPage",
+    component: LocationPage,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
 });
 
 export default router;
